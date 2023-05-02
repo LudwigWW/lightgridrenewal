38e89d6bca743b9db47a591f8b0a8870ef0a2cce
Ludwig Wilhelm Wall <lwall@uwaterloo.ca>
2023-05-02 16:13:08 +0200

    V1.0 min viable exhibition

--- example/Game.py
+++ example/Game.py
@@ -2,14 +2,16 @@
 import pygame, sys
 from pygame.locals import *
 import random, time
+from math import sin, radians
 
 #Initialzing 
 pygame.init()
 
 #Setting up FPS 
-FPS = 60
+FPS = 600
 FramePerSec = pygame.time.Clock()
 counter = 0
+eventCounter = 0
 
 #Creating colors
 BLUE  = (0, 0, 255)
@@ -32,7 +34,9 @@ game_over = font.render("Game Over", True, BLACK)
 background = pygame.image.load("AnimatedStreet.png")
 foreground = pygame.image.load("Foreground.png")
 # foregroundCenter = (SCREEN_WIDTH/2, SCREEN_HEIGHT-foreground.get_rect().height/2)
-
+obstacleImage = pygame.image.load("Enemy.png")
+bushImage = pygame.image.load("Bush1.png")
+treeImage = pygame.image.load("Tree1.png")
 
 #Create a white screen 
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -64,18 +68,163 @@ class Road(pygame.sprite.Sprite):
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__() 
-        self.image = pygame.image.load("Enemy.png")
+        super().__init__()
+        self.reset()
+
+    def reset(self):
+        self.scale = 5
+        self.image = pygame.transform.scale(obstacleImage, (self.scale, self.scale))
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
+        self.randomStart = random.randint(0,50)
+        self.rect.center = (SCREEN_WIDTH/2+self.randomStart-25, 240)
+        self.angle = abs((self.randomStart-25)/20)
+        self.stepCounter = 10
+        self.moveX = 0
+        self.moveY = 0
+        self.leftright = 1
+        if (self.randomStart < 25):
+            self.leftright = -1
+        self.rect.top = 240
 
     def move(self):
         global SCORE
-        self.rect.move_ip(0,SPEED/2)
+        self.stepCounter = self.stepCounter*1.02
+
+        self.moveX += self.stepCounter*self.angle*SPEED
+        self.moveY += self.stepCounter*SPEED
+        self.scale = 5+0.05*self.stepCounter*SPEED
+        
+
+        pixelsX = self.leftright*(self.moveX // 500)
+        if (self.moveX > 500):
+            self.moveX -= self.leftright*pixelsX*500
+            # self.rect = self.image.get_rect()
+
+        pixelsY = self.moveY // 500
+        if (self.moveY > 500):
+            self.moveY -= pixelsY*500
+            self.image = pygame.transform.scale(obstacleImage, (self.scale, self.scale))
+
+        # if (self.leftright):
+        self.rect.move_ip(pixelsX,pixelsY)
+        # else:
+        #     self.rect.move_ip(-pixelsX,pixelsY)
         if (self.rect.bottom > (SCREEN_HEIGHT+100)):
             SCORE += 1
-            self.rect.top = 0
-            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+            self.reset()
+            
+
+class RoadDecor(pygame.sprite.Sprite):
+    def __init__(self, variation):
+        super().__init__()
+        self.variation = variation
+        
+        self.reset()
+        
+    def reset(self):
+        self.scale = 5
+        self.image = pygame.transform.scale(bushImage, (self.scale*2.5, self.scale))
+        self.rect = self.image.get_rect()
+        self.randomStart = random.randint(0,50)
+        
+        self.angle = 4+abs((self.randomStart-25)/10)
+        self.stepCounter = 10
+        self.moveX = 0
+        self.moveY = 0
+        self.leftright = 1
+        if (self.randomStart < 25):
+            self.leftright = -1
+            self.angle += 2.5
+        # self.rect.top = 240
+        self.rect.center = (SCREEN_WIDTH/2+self.randomStart-25+60*self.leftright, 240+self.variation)
+
+    def move(self):
+        global SCORE
+        self.stepCounter = self.stepCounter*1.02
+
+        self.moveX += self.stepCounter*self.angle*SPEED
+        self.moveY += self.stepCounter*SPEED
+        self.scale = 2+0.4*self.stepCounter
+        
+
+        pixelsX = self.leftright*(self.moveX // 1000)
+        if (self.moveX > 1000):
+            self.moveX -= self.leftright*pixelsX*1000
+            # self.rect = self.image.get_rect()
+
+        pixelsY = self.moveY // 1000
+        if (self.moveY > 1000):
+            self.moveY -= pixelsY*1000
+            self.image = pygame.transform.scale(bushImage, (self.scale*2.5, self.scale))
+
+        # if (self.leftright):
+        self.rect.move_ip(pixelsX,pixelsY)
+        # else:
+        #     self.rect.move_ip(-pixelsX,pixelsY)
+        if (self.rect.bottom > (SCREEN_HEIGHT+100)):
+            SCORE += 1
+            self.reset()
+
+
+class BGDecor(pygame.sprite.Sprite):
+    def __init__(self, variation):
+        super().__init__()
+        self.variation = variation
+        
+        self.reset()
+        
+    def reset(self):
+        self.scale = 5
+        self.image = pygame.transform.scale(treeImage, (self.scale, self.scale*7))
+        self.rect = self.image.get_rect()
+        self.randomStart = random.randint(0,50)
+        
+        self.angle = 20+abs((self.randomStart-25)/10)
+        self.stepCounter = 10
+        self.moveX = 0
+        self.moveY = 0
+        self.appearRate = 100
+        self.leftright = 1
+        if (self.randomStart < 25):
+            self.leftright = -1
+        # self.rect.top = 240
+        self.rect.center = (SCREEN_WIDTH/2+(self.randomStart-30)*0.2+40*self.leftright, 265-self.variation)
+
+    def move(self):
+        global SCORE
+        self.stepCounter = self.stepCounter*1.02
+
+        self.moveX += self.stepCounter*SPEED
+        self.moveY += 0.1*self.stepCounter*self.angle*SPEED
+        # self.scale += 0.1*self.stepCounter*SPEED
+        # if (self.rect.left > 140 and self.rect.left < 260): # in center area
+        #     self.moveY += self.appearRate*SPEED
+        #     self.appearRate -= 1
+
+        pixelsY = self.moveY // 1000
+        if (self.moveY > 1000):
+            self.moveY -= pixelsY*1000
+            # self.image = pygame.transform.scale(treeImage, (self.scale, self.scale*7))
+
+        pixelsX = self.leftright*(self.moveX // 1000)
+        if (self.moveX > 1000):
+            self.moveX -= self.leftright*pixelsX*1000
+            self.scale += 0.1*self.leftright*pixelsX
+            self.image = pygame.transform.scale(treeImage, (self.scale, self.scale*7))
+
+            self.angle = self.angle * 0.98**(self.leftright*pixelsX)
+            # self.rect = self.image.get_rect()
+
+
+        # if (self.leftright):
+        self.rect.move_ip(pixelsX,-pixelsY)
+        # else:
+        #     self.rect.move_ip(-pixelsX,pixelsY)
+
+
+        if (self.rect.left > (SCREEN_WIDTH+50) or self.rect.left < -50):
+            SCORE += 1
+            self.reset()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -89,7 +238,7 @@ class Player(pygame.sprite.Sprite):
     def move(self):
         pressed_keys = pygame.key.get_pressed()
         
-        if self.rect.left > -50:
+        if self.rect.left > -200:
               if pressed_keys[K_LEFT]:
                   self.rect.move_ip(-5, 0)
         if self.rect.right < SCREEN_WIDTH:        
@@ -135,6 +284,10 @@ class Background(pygame.sprite.Sprite):
 #Setting up Sprites        
 P1 = Player()
 E1 = Enemy()
+Decor1 = RoadDecor(5)
+Decor2 = RoadDecor(10)
+Decor3 = RoadDecor(0)
+
 
 #Setting up Background
 BG = Background(False)
@@ -149,11 +302,21 @@ enemies.add(E1)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(E1)
+all_sprites.add(Decor1)
+all_sprites.add(Decor2)
+all_sprites.add(Decor3)
 # all_sprites.add(BG)
 
+
 bg_sprites = pygame.sprite.Group()
+
 bg_sprites.add(BG)
 bg_sprites.add(BG2)
+
+
+bg_decor_sprites = pygame.sprite.Group()
+Tree1 = BGDecor(random.randint(0,10))
+bg_decor_sprites.add(Tree1)
 
 #Adding a new User event 
 INC_SPEED = pygame.USEREVENT + 1
@@ -168,7 +331,7 @@ while True:
             SPEED += 1      
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit()            
 
 
     DISPLAYSURF.blit(background, (0,0))
@@ -183,6 +346,10 @@ while True:
     
     for entity in bg_sprites:
         entity.reset()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+
+    for entity in bg_decor_sprites:
+        entity.move()
         DISPLAYSURF.blit(entity.image, entity.rect)
 
     DISPLAYSURF.blit(foreground, (7,10))
@@ -215,10 +382,18 @@ while True:
         sys.exit()        
         
 
-    if (counter > 100):
-        print(FramePerSec.get_fps())
-        counter = 0
-    else:
-        counter += 1
+    if (eventCounter < 10):
+        if (counter > 20):
+            print(FramePerSec.get_fps())
+            counter = 0
+            if (eventCounter < 10):
+                eventCounter += 1
+                newBush = RoadDecor(random.randint(0,10))
+                all_sprites.add(newBush)
+                newTree = BGDecor(random.randint(0,10))
+                bg_decor_sprites.add(newTree)
+        else:
+            counter += 1
+        
     pygame.display.update()
     FramePerSec.tick(FPS)
